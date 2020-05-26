09de81f6cc46a94b4f17c7d81c7effee95c70a6f
Tran Nguyen Duc Tho <thorius@pop-os.localdomain>
2020-05-26 19:53:50 +0700

    Add play video

---
+++ play_video.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 23 10:07:50 2020
+
+@author: thorius
+"""
+import vlc
+import logging
+
+
+class PlayVideo():
+    def __init__(self, 
+                 path_video_demo_folder = './video_demo/'):
+        
+        # create logger to debug
+        self.logger = logging.getLogger()
+        self.logger.setLevel(logging.INFO)
+
+        self.path_video_demo_folder = path_video_demo_folder
+        
+    def settings(self):
+        self.instance_vlc = vlc.Instance()
+        self.media_player = self.instance_vlc.media_player_new()
+    
+    
+    def setChannel(self, index_channel):
+        
+        self.Media = self.instance_vlc.media_new(self.path_video_demo_folder  + str(index_channel) + '.mp4')
+        self.Media.get_mrl()
+    
+    def getIndexChannel(self, channel_list):
+        index_channel = -1
+        for i in range(len(channel_list)):
+            if channel_list[i] == 1:
+                index_channel = i
+        return index_channel
+     
+    
+    def setConfig(self, enable_fullscreen = False):
+        
+        self.media_player.set_media(self.Media)
+        self.media_player.set_fullscreen(enable_fullscreen)
+        
+        
+            
+    def startPlayVideo(self):
+        self.media_player.play()
+        
+        
+    def stopPlayVideo(self):
+        self.media_player.stop()
+        
+        
+    def setControlList(self, control_list, channel_list, keymax_control_predictions):
+        
+         
+        
+        currentChannel = self.getIndexChannel(channel_list)
+        
+        
+        if keymax_control_predictions == 2:
+            control_list[0] = 1
+            control_list[1] = 0
+        elif keymax_control_predictions == 3:
+            control_list[0] = 0
+            control_list[1] = 1
+            
+        elif keymax_control_predictions == 4:
+            channel_list[currentChannel] = 0
+            
+            if currentChannel == 9:
+                channel_list[0] = 1
+            else:
+                channel_list[currentChannel+1] = 1
+                
+        elif keymax_control_predictions == 5:
+            channel_list[currentChannel] = 0
+            channel_list[currentChannel-1] = 1
+        elif keymax_control_predictions > 5 and currentChannel + 6 != keymax_control_predictions:
+            channel_list[currentChannel] = 0
+            channel_list[keymax_control_predictions-6] = 1
+        
+        return control_list, channel_list
+    
+    
+    
+    def setDefaultList(self):
+        control_list = [1, 0]
+        channel_list = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        
+        
+        return control_list, channel_list
+        
+        
+            
+            
+if __name__ == '__main__':
+    py_vd = PlayVideo()
+    py_vd.settings()
